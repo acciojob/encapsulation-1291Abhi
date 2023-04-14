6d885cb91db21cfa0efbc870dcb4b33c8903888e
Abhinesh Singh <abhisingh1291@gmail.com>
2023-04-14 13:57:13 +0530

    Creating object of RWOnly and implementing getter and setter

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String args[]){
+      RWOnly rw=new RWOnly();
+
+     // rw.name="Abhi";
+     // System.out.println(rw.name);
+      //getting error while printing and setting name here
+      //7:9
+      //java: name has private access in com.driver.RWOnly
+      rw.setName("Abhi");
+      String name=rw.getName();
+
+  }
 }
